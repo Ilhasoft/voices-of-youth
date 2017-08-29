1845b66d3867d570375fa56720178f13369fafe1
Eduardo Douglas <edudouglas@gmail.com>
2017-08-29 11:52:34 -0300

    Added TagAdmin

--- voicesofyouth/tags/admin.py
+++ voicesofyouth/tags/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Tag
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('country_name', 'name', 'system_tag', 'urgency_score')
+
+    def country_name(self, obj):
+        return obj.country.name
+
+
+admin.site.register(Tag, TagAdmin)
