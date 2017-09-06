80db164439d793777d7a61252337ef4ef8088238
Eduardo Douglas <edudouglas@gmail.com>
2017-09-06 15:21:43 -0300

    Added session auth, rest documentation

--- voicesofyouth/settings.py
+++ voicesofyouth/settings.py
@@ -157,6 +157,7 @@ REST_FRAMEWORK = {
     'PAGINATE_BY': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
