e4ba773f702240ae033a73a8b6bcaf6083a054fc
eltonplima <eltonplima@gmail.com>
2017-10-04 18:31:57 -0300

    Update bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 VENV_NAME=${1:-"env"}
 WORKSPACE=${WORKSPACE:-$PWD}
-VENV_PATH=$WORKSPACE/$VENV_NAME
+VENV_PATH=$WORKSPACE/${VENV_NAME}
 PROJECT_NAME=`find . -not -path '*/\.*' -iname settings.py | cut -d \/ -f 2`
 
 create_venv() {
-    if [ ! -d "$VENV_PATH" ]; then
+    if [ ! -d "${VENV_PATH}" ]; then
         echo "Creating a new virtual environment..."
-        virtualenv -p python3 $VENV_NAME
+        virtualenv -p python3 ${VENV_NAME}
     fi
     # The virtualenv has been created with success?
     if [ $? != 0 ]; then
         echo "Somethings went wrong! Try again!"
-        rm $VENV_PATH -rf
+        rm ${VENV_PATH} -rf
         return -1
     fi
 }
 
 validate_venv() {
     # Sanity check
-    if [[ (! -f $VENV_PATH/bin/python) || (! -f $VENV_PATH/bin/activate) ]]; then
+    if [[ (! -f ${VENV_PATH}/bin/python) || (! -f ${VENV_PATH}/bin/activate) ]]; then
         echo "The virtualenv has been corrupted. Try again!"
-        rm $VENV_PATH -rf
+        rm ${VENV_PATH} -rf
         return -1
     fi
 }
 
 bootstrap() {
-    source $VENV_PATH/bin/activate && {
+    source ${VENV_PATH}/bin/activate && {
         pip install -r development.txt
     }
 }
 
 install_deps() {
-    source $VENV_PATH/bin/activate && {
+    source ${VENV_PATH}/bin/activate && {
         pip install -r development.txt
     }
 }
@@ -43,7 +43,7 @@ init_git() {
         git init
     fi
     if [ ! -f ".gitignore" ]; then
-        printf $VENV_NAME"\n*.pyc\nstatic\n*.sqlite3" > .gitignore
+        printf ${VENV_NAME}"\n*.pyc\nstatic\n*.sqlite3" > .gitignore
     fi
 }
 
