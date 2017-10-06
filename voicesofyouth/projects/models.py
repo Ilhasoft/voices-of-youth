265d1fa6be69392f74096d27869f2a3e4fcd8e05
eltonplima <eltonplima@gmail.com>
2017-10-06 13:15:47 -0300

    Refactory Project model.
    path is optional. When save, if None we use slugify version of project.name.
    Added docstring.

--- voicesofyouth/projects/models.py
+++ voicesofyouth/projects/models.py
@@ -2,6 +2,9 @@ from django.conf import settings as django_settings
 from django.db import models
 from django.contrib.gis.db import models as gismodels
 from django.utils.translation import ugettext_lazy as _
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
 
 from voicesofyouth.core.models import BaseModel
 
@@ -15,9 +18,28 @@ USER_CHOICES = (
 
 
 class Project(BaseModel):
-    name = models.CharField(max_length=100, null=False, blank=False, verbose_name=_('Name'))
-    path = models.CharField(max_length=100, null=False, blank=False, verbose_name=_('Path'))
-    language = models.CharField(max_length=90, choices=django_settings.LANGUAGES, default='en')
+    '''
+    The project can aggregate many types of study themes.
+
+    For example: We can create a project called Brasil issues, and inside of this we can create many themes, like
+    waste issues, health issues.
+
+    Another example is a global climate changes, we create a project called Global Climate Changes with one big theme.
+
+    Args:
+        name: Name of project.
+        path: URL path for the project. The default value is slug of name.
+        language: Default language. If the user doesn't set the main language we use that language.
+    '''
+    name = models.CharField(max_length=100, verbose_name=_('Name'))
+    path = models.CharField(max_length=100,
+                            null=True,
+                            blank=True,
+                            verbose_name=_('URL Path'))
+    language = models.CharField(max_length=90,
+                                choices=django_settings.LANGUAGES,
+                                default='en',
+                                verbose_name=_('Language'))
 
     def __str__(self):
         return self.name
@@ -25,7 +47,7 @@ class Project(BaseModel):
 
 class Setting(BaseModel):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    location = gismodels.PolygonField(null=False, blank=False)
+    location = gismodels.PolygonField()
 
     def __str__(self):
         return self.project.name
@@ -63,3 +85,13 @@ class ProjectUsers(BaseModel):
         verbose_name = _('Projects Users')
         verbose_name_plural = _('Projects Users')
         db_table = 'projects_project_users'
+
+
+###############################################################################
+# Signals handlers
+###############################################################################
+
+@receiver(pre_save, sender=Project)
+def set_project_path(sender, instance, **kwargs):
+    if not instance.path:
+        instance.path = slugify(instance.name)
--- voicesofyouth/projects/tests.py
+++ voicesofyouth/projects/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.utils.text import slugify
 
-# Create your tests here.
+from model_mommy import mommy
+
+from .models import Project
+
+
+class ProjectTestCase(TestCase):
+    def setUp(self):
+        self.project_name = 'Name of project'
+        self.project = mommy.make(Project, name=self.project_name)
+
+    def test_set_default_path(self):
+        self.assertEqual(self.project.path, slugify(self.project.name))
+
+    def test__str__(self):
+        self.assertEqual(str(self.project), self.project_name)
