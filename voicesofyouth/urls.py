c650885bcbd02b97761d839d47168d10c9222087
eltonplima <eltonplima@gmail.com>
2017-10-18 18:02:41 -0300

    Update urls.py
    Added support to access media files.

--- voicesofyouth/urls.py
+++ voicesofyouth/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('voicesofyouth.api.v1.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
