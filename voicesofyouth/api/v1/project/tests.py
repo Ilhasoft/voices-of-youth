8739cd68a6ba97e9215bc63aebbbc0340e5e7f7a
eltonplima <eltonplima@gmail.com>
2017-10-20 16:01:15 -0300

    Update project API test.

--- voicesofyouth/api/v1/project/tests.py
+++ voicesofyouth/api/v1/project/tests.py
@@ -1,28 +1,33 @@
 import tempfile
 
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from model_mommy import mommy
-from unipath import Path
 from PIL import Image
+from model_mommy import mommy
+from rest_framework import status
+from rest_framework.reverse import reverse_lazy
+from rest_framework.test import APITestCase
 
 from voicesofyouth.projects.models import Project
 from voicesofyouth.users.models import User
 
 
+def create_fake_image():
+    img = Image.new('RGB', (100, 100), 255)
+    tmp_img = tempfile.NamedTemporaryFile(suffix='.jpg')
+    img.save(tmp_img)
+    tmp_img.seek(0)
+    return tmp_img
+
+
 class ProjectTestCase(APITestCase):
     @classmethod
     def setUpClass(cls):
-        img = Image.new('RGB', (100, 100), 255)
-        tmp_img = tempfile.NamedTemporaryFile(suffix='.jpg')
-        img.save(tmp_img)
-        tmp_img.seek(0)
         cls.data = {
             'name': 'Project X',
-            'thumbnail': tmp_img
+            'thumbnail': create_fake_image()
         }
-        cls.url = reverse('projects-list')
+        cls.project = mommy.make(Project)
+        cls.url_list = reverse_lazy('projects-list')
+        cls.url_detail = reverse_lazy('projects-detail', args=[cls.project.id, ])
         cls.admin_credentials = {'username': 'admin', 'password': 'Un1c3f@@'}
         cls.user_credentials = {'username': 'user', 'password': 'user'}
         cls.admin = User.objects.get(username='admin')
@@ -32,26 +37,21 @@ class ProjectTestCase(APITestCase):
     def tearDownClass(cls):
         pass
 
+    def setUp(self):
+        self.client.login(**self.admin_credentials)
+
+    def tearDown(self):
+        self.client.logout()
+
     def test_get_without_data(self):
-        response = self.client.get(self.url)
-        self.assertEqual(Project.objects.count(), 0)
+        response = self.client.get(self.url_list)
+        self.assertEqual(Project.objects.count(), 1)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
+        self.assertEqual(len(response.data), 1)
 
     def test_get_with_data(self):
         mommy.make(Project, 10)
-        response = self.client.get(self.url)
-        self.assertEqual(Project.objects.count(), 10)
+        response = self.client.get(self.url_list)
+        self.assertEqual(Project.objects.count(), 11)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 10)
-
-    def test_post_without_authentication(self):
-        response = self.client.post(self.url, self.data)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_post_with_admin_authentication(self):
-        self.client.login(**self.admin_credentials)
-        self.assertEqual(Project.objects.count(), 0)
-        response = self.client.post(self.url, self.data, format='multipart')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Project.objects.count(), 1)
+        self.assertEqual(len(response.data), 11)
