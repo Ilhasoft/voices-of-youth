e77b66b5fd75457b4d292d88aec1ab3bac1a04da
eltonplima <eltonplima@gmail.com>
2017-10-20 16:01:30 -0300

    Organize import.

--- voicesofyouth/themes/models.py
+++ voicesofyouth/themes/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import Group
 from django.contrib.gis.db import models as gismodels
 from django.core.validators import MinLengthValidator
 from django.db import models
-from django.db.models.signals import post_save
 from django.db.models.signals import m2m_changed
+from django.db.models.signals import post_save
 from django.db.models.signals import pre_save
 from django.dispatch.dispatcher import receiver
 from django.utils.translation import ugettext_lazy as _
