c0689f2a36cfce9c921a6198bffcb28127d56926
eltonplima <eltonplima@gmail.com>
2017-10-23 15:25:05 -0300

    Removed unnecessary files.

--- voicesofyouth/api/v1/project/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import permissions
-
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.created_by == request.user
