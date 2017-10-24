a81177dd9d133a41f45860fb70344a34cff14f29
eltonplima <eltonplima@gmail.com>
2017-10-24 16:15:46 -0300

    Implemented translation for Theme API.

--- voicesofyouth/api/v1/theme/views.py
+++ voicesofyouth/api/v1/theme/views.py
@@ -1,8 +1,12 @@
-from rest_framework import viewsets, permissions
+from rest_framework import permissions
+from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from voicesofyouth.api.v1.theme.serializers import ThemeSerializer
 from voicesofyouth.api.v1.theme.serializers import ThemeTranslationSerializer
 from voicesofyouth.theme.models import Theme, ThemeTranslation
+from voicesofyouth.translation.models import Translation
 
 
 class ThemesViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,12 +18,25 @@ class ThemesViewSet(viewsets.ReadOnlyModelViewSet):
     Return a list of all the existing themes by map.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
+    queryset = Theme.objects.all()
     serializer_class = ThemeSerializer
 
     def get_queryset(self):
+        filter_clause = {}
+        project_id = self.request.query_params.get('project', 0)
+        if project_id:
+            filter_clause['project__id'] = project_id
+
         return Theme.objects.filter(is_active=True,
                                     visible=True,
-                                    project__id=self.request.query_params.get('project', 0))
+                                    **filter_clause)
+
+    def retrieve(self, request, pk=None):
+        lang = self.request.query_params.get('lang', '').strip()
+        theme = get_object_or_404(self.queryset, pk=pk)
+        Translation.translate_object(theme, lang)
+        serializer = self.serializer_class(theme, context={'request': request})
+        return Response(serializer.data)
 
 
 class ThemeTranslationViewSet(viewsets.ReadOnlyModelViewSet):
