190eddb82fb2d0fd3ec75e7c4e9df2f2bd198b16
eltonplima <eltonplima@gmail.com>
2017-10-24 11:11:57 -0300

    Update admin.

--- voicesofyouth/translation/admin.py
+++ voicesofyouth/translation/admin.py
@@ -4,6 +4,22 @@ from voicesofyouth.translation.models import TranslatableModel
 from voicesofyouth.translation.models import TranslatableField
 from voicesofyouth.translation.models import Translation
 
-admin.site.register(TranslatableModel)
-admin.site.register(TranslatableField)
-admin.site.register(Translation)
+
+class TranstableModelAdmin(admin.ModelAdmin):
+    list_display = ('model', 'verbose_name', 'verbose_name_plural')
+    readonly_fields = list_display
+
+
+class TranslatableFieldAdmin(admin.ModelAdmin):
+    list_display = ('model', 'field', 'verbose_name')
+    readonly_fields = list_display
+
+
+class TranslationAdmin(admin.ModelAdmin):
+    list_display = ('field', 'language', 'content_object', 'translation')
+    list_editable = ('language', 'translation')
+
+
+admin.site.register(TranslatableModel, TranstableModelAdmin)
+admin.site.register(TranslatableField, TranslatableFieldAdmin)
+admin.site.register(Translation, TranslationAdmin)
