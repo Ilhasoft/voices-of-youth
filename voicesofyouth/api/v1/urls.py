360f04c45c250f88c0e129a1c86ea4e94f34b7a8
eltonplima <eltonplima@gmail.com>
2017-10-26 13:51:05 -0300

    Removed app maps.

--- voicesofyouth/api/v1/urls.py
+++ voicesofyouth/api/v1/urls.py
@@ -6,19 +6,15 @@ from rest_framework.routers import DefaultRouter
 from voicesofyouth.api.v1.report.views import ReportsViewSet
 from .project.views import ProjectsViewSet
 from .theme.views import ThemesViewSet
-from .views import CommentsEndPoint
-from .views import MapsEndPoint
 from .views import TagsViewSet
 from .views import UsersEndPoint
 
 router = DefaultRouter()
 router.register(r'projects', ProjectsViewSet, base_name='projects')
-router.register(r'maps', MapsEndPoint, base_name='maps')
 router.register(r'themes', ThemesViewSet, base_name='themes')
 router.register(r'tags', TagsViewSet, base_name='tags')
 router.register(r'users', UsersEndPoint, base_name='users')
 router.register(r'reports', ReportsViewSet, base_name='reports')
-router.register(r'comments', CommentsEndPoint, base_name='comments')
 
 urlpatterns = [
     url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token'),
--- voicesofyouth/api/v1/views.py
+++ voicesofyouth/api/v1/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import mixins, viewsets
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from voicesofyouth.api.v1.serializers import TagSerializer
-from voicesofyouth.maps.models import Map
 from voicesofyouth.tag.models import Tag
 from voicesofyouth.theme.models import Theme
 from voicesofyouth.users.models import User
-from .serializers import CommentSerializer
-from .serializers import MapSerializer
 from .serializers import UserSerializer
 
 
@@ -25,41 +22,6 @@ class TagsViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         theme = get_object_or_404(Theme, pk=self.request.query_params.get('theme', 0))
         return Tag.objects.filter(object_id=theme.id)
-
-
-class MapsEndPoint(viewsets.ReadOnlyModelViewSet):
-    """
-    retrieve:
-    Return the given map and related themes.
-
-    list:
-    Return a list of all the existing maps.
-    """
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    serializer_class = MapSerializer
-    queryset = Map.objects.all()
-
-    # def get_queryset(self):
-    #     return Map.objects.filter(is_active=True).filter(id=self.kwargs['pk'])
-    #
-    # def retrieve(self, request, *args, **kwargs):
-    #     self.serializer_class = MapAndThemesSerializer
-    #     instance = self.get_object()
-    #     serializer = self.get_serializer(instance)
-    #     return Response(serializer.data)
-
-
-class CommentsEndPoint(mixins.CreateModelMixin,
-                       viewsets.GenericViewSet):
-    """
-    create:
-    Create a new comment.
-    """
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-    serializer_class = CommentSerializer
-
-    def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user, modified_by=self.request.user)
 
 
 class UsersEndPoint(viewsets.ReadOnlyModelViewSet):
