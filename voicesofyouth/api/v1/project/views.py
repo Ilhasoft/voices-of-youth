28e9cc9b19bcbbc0f746174d392a9fbf74bdd5bb
eltonplima <eltonplima@gmail.com>
2017-11-01 15:52:14 -0300

    Refactory and documentation of project endpoint.
    Removed retrieve method.

--- voicesofyouth/api/v1/project/views.py
+++ voicesofyouth/api/v1/project/views.py
@@ -1,24 +1,19 @@
 from rest_framework import permissions, viewsets
-from rest_framework.generics import get_object_or_404
-from rest_framework.response import Response
 
 from voicesofyouth.project.models import Project
-from voicesofyouth.translation.models import Translation
 from .serializers import ProjectSerializer
 
 
 class ProjectsViewSet(viewsets.ReadOnlyModelViewSet):
     """
+    Return a list of projects.
+
+    You can use the querystring to get the translated version of the project. E.g. to get a project in portuguese
+    brazilian just use: ?lang=pt-br
+
     retrieve:
-    You can use querystring to translate the project. E.g.: /api/projects/1/?lang=pt-br
+    Return a specific project.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-
-    def retrieve(self, request, pk=None):
-        lang = self.request.query_params.get('lang', '').strip()
-        project = get_object_or_404(self.queryset, pk=pk)
-        Translation.objects.translate_object(project, lang)
-        serializer = self.serializer_class(project)
-        return Response(serializer.data)
