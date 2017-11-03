86d510d524cf4827bc4858a0c89bc0d156a3c22d
eltonplima <eltonplima@gmail.com>
2017-11-03 14:06:38 -0300

    Update serializer avatar field.

--- voicesofyouth/api/v1/user/serializers.py
+++ voicesofyouth/api/v1/user/serializers.py
@@ -4,7 +4,12 @@ from voicesofyouth.user.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    avatar = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'language', 'avatar', 'username', 'is_mapper')
+
+    def get_avatar(self, obj):
+        request = self.context['request']
+        return request.build_absolute_uri(obj.get_avatar_display())
