b02d54ef9150b30ed2afd144a974b2f7672a21da
eltonplima <eltonplima@gmail.com>
2017-11-07 15:01:21 -0300

    Tags validation for report.
    The report only accept tags that has been linked with theme or project of that report.

--- voicesofyouth/tag/models.py
+++ voicesofyouth/tag/models.py
@@ -1,8 +1,15 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.db.models.query_utils import Q
 from django.utils.functional import cached_property
 from django.utils.translation import ugettext_lazy as _
+from taggit.managers import _TaggableManager
 from taggit.models import CommonGenericTaggedItemBase
 from taggit.models import TaggedItemBase
+
+
+class InvalidTag(Exception):
+    pass
 
 
 class Tag(CommonGenericTaggedItemBase, TaggedItemBase):
@@ -12,3 +19,17 @@ class Tag(CommonGenericTaggedItemBase, TaggedItemBase):
     @cached_property
     def name(self):
         return self.tag.name
+
+
+class _ReportTaggableManager(_TaggableManager):
+    def _to_tag_model_instances(self, tags):
+        tag_objs = super(_ReportTaggableManager, self)._to_tag_model_instances(tags)
+        instance = self.instance
+        ct_project = ContentType.objects.get_for_model(instance.theme.project._meta.model)
+        ct_theme = ContentType.objects.get_for_model(instance.theme._meta.model)
+        qs_tags = Tag.objects.filter(Q(content_type=ct_theme, object_id=instance.theme.id) |
+                                     Q(content_type=ct_project, object_id=instance.theme.project.id))
+        for tag in tag_objs:
+            if not qs_tags.filter(tag__name=tag).exists():
+                raise InvalidTag()
+        return tag_objs
--- voicesofyouth/tag/tests.py
+++ voicesofyouth/tag/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
-
 from model_mommy import mommy
 from taggit.models import CommonGenericTaggedItemBase
 from taggit.models import TaggedItemBase
 
+from voicesofyouth.project.models import Project
+from voicesofyouth.report.models import Report
+from voicesofyouth.tag.models import InvalidTag
 from voicesofyouth.tag.models import Tag
+from voicesofyouth.theme.models import Theme
 
 
 class TagTestCase(TestCase):
@@ -18,3 +21,38 @@ class TagTestCase(TestCase):
 
     def test_base_classes(self):
         self.assertTrue(issubclass(Tag, (CommonGenericTaggedItemBase, TaggedItemBase)))
+
+
+class ReportTagManager(TestCase):
+    def test_with_valid_theme_tag(self):
+        self.assertEqual(Tag.objects.count(), 0)
+        theme = mommy.make(Theme)
+        theme.tags.add('tag_theme')
+        report = mommy.make(Report, theme=theme)
+        report.tags.add('tag_theme')
+        self.assertEqual(report.tags.count(), 1)
+        self.assertEqual(Tag.objects.count(), 2)
+
+    def test_with_valid_project_tag(self):
+        self.assertEqual(Tag.objects.count(), 0)
+        project = mommy.make(Project)
+        project.tags.add('tag_project')
+        theme = mommy.make(Theme, project=project)
+        theme.tags.add('tag_theme')
+        report = mommy.make(Report, theme=theme)
+        report.tags.add('tag_theme', 'tag_project')
+        self.assertEqual(report.tags.count(), 2)
+        self.assertEqual(Tag.objects.count(), 4)
+
+    def test(self):
+        self.assertEqual(Tag.objects.count(), 0)
+        project = mommy.make(Project)
+        project.tags.add('tag_project')
+        theme = mommy.make(Theme, project=project)
+        theme.tags.add('tag_theme')
+        report = mommy.make(Report, theme=theme)
+        report.tags.add('tag_theme', 'tag_project')
+        with self.assertRaises(InvalidTag):
+            report.tags.add('invalid_tag')
+        self.assertEqual(report.tags.count(), 2)
+        self.assertEqual(Tag.objects.count(), 4)
