c207f3f123bc5f55bf88ab87bb951dd2a694a849
eltonplima <eltonplima@gmail.com>
2017-11-30 17:35:13 -0300

    Rename url property name.

--- voicesofyouth/user/urls.py
+++ voicesofyouth/user/urls.py
@@ -6,7 +6,7 @@ from voicesofyouth.user.view import MappersListView
 
 urlpatterns = [
     url(r'^admins/$', AdminListView.as_view(), name='admins_list'),
-    url(r'^admins/(?P<mapper_id>[0-9]+)/$', AdminDetailView.as_view(), name='admin_detail'),
+    url(r'^admins/(?P<admin_id>[0-9]+)/$', AdminDetailView.as_view(), name='admin_detail'),
     url(r'^mappers/(?P<mapper_id>[0-9]+)/$', MapperDetailView.as_view(), name='mapper_detail'),
     url(r'^mappers/$', MappersListView.as_view(), name='mappers_list'),
 ]
