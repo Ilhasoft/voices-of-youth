2f18584daaaae4f3e9a518df7271659e4d211027
eltonplima <eltonplima@gmail.com>
2017-12-14 14:04:17 -0300

    Refactory users endpoint.
    If authenticated user is admin, return all users.
    If authenticated user is admin and send the theme id in request, we return all mappers from that theme.

--- voicesofyouth/api/v1/user/views.py
+++ voicesofyouth/api/v1/user/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth.models import AnonymousUser
 from rest_framework import viewsets
+from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from voicesofyouth.api.v1.user.serializers import UserSerializer
+from voicesofyouth.theme.models import Theme
 from voicesofyouth.user.models import User
 
 
@@ -11,5 +14,15 @@ class UsersEndPoint(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         token = self.request.query_params.get('auth_token')
+        theme = self.request.query_params.get('theme')
+        user = self.request.user
+
+        if not isinstance(user, AnonymousUser):
+            if user.is_admin:
+                if theme:
+                    theme = get_object_or_404(Theme, id=theme)
+                    return theme.mappers_group.user_set.all()
+                else:
+                    return User.objects.all()
         if token:
             return User.objects.filter(auth_token=token)
