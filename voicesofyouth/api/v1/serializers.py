3ba621851baae8d74a1187d6670107b7de6b04ee
eltonplima <eltonplima@gmail.com>
2017-12-14 14:12:53 -0300

    Refactory VoySerializer.
    If an admin user send the parameter mapper_id, then created_by and modified_by will be set to that user.

--- voicesofyouth/api/v1/serializers.py
+++ voicesofyouth/api/v1/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
+from rest_framework.generics import get_object_or_404
+
+from voicesofyouth.user.models import User
 
 
 class VoySerializer(serializers.HyperlinkedModelSerializer):
     def create(self, validated_data):
         request = self.context.get('request')
         user = request.user
-        validated_data['created_by'] = user
-        validated_data['modified_by'] = user
+        mapper_id = request.data.get('mapper_id')
+        if mapper_id and user.is_admin:
+            mapper = get_object_or_404(User, id=mapper_id)
+            validated_data['created_by'] = mapper
+            validated_data['modified_by'] = mapper
+        else:
+            validated_data['created_by'] = user
+            validated_data['modified_by'] = user
         return super().create(validated_data)
