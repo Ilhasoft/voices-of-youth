135c3236709b775fb3475331088c120a8c7bd1ed
Eduardo Douglas <edudouglas@gmail.com>
2018-02-01 16:16:31 -0300

    Added user projects endpoint

--- voicesofyouth/api/v1/project/views.py
+++ voicesofyouth/api/v1/project/views.py
@@ -1,6 +1,7 @@
 from rest_framework import permissions, viewsets
 
 from voicesofyouth.project.models import Project
+from voicesofyouth.user.models import MapperUser
 from .serializers import ProjectSerializer
 
 
@@ -10,11 +11,19 @@ class ProjectsViewSet(viewsets.ReadOnlyModelViewSet):
     Return a list of projects.
 
     You can use the querystring to get the translated version of the project. E.g. to get a project in portuguese
-    brazilian just use: ?lang=pt-br. If the requested translation does not exists you will receive the default language.
+    brazilian just use: ?lang=pt-br. If the requested translation does not exists you will receive the default language. Returns list of projects that user id is associated, just use: ?auth_token=user_token
 
     retrieve:
     Return a specific project.
     """
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, ]
-    queryset = Project.objects.all().filter(enabled=True)
     serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+        token = self.request.query_params.get('auth_token')
+
+        if token:
+            user = MapperUser.objects.filter(auth_token=token).first()
+            return user.projects.all()
+
+        return Project.objects.all().filter(enabled=True)
