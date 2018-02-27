1062b0f181216a534a6feac57a0f68ca5b0976ba
Eduardo Douglas <edudouglas@gmail.com>
2018-02-27 17:34:45 -0300

    Translate login

--- frontend/src/translate/languages.js
+++ frontend/src/translate/languages.js
@@ -20,6 +20,33 @@ const messages = {
           title: 'Back to map',
         },
       },
+      pages: {
+        login: {
+          title: 'Hello, again!',
+          subtitle: 'Log in to your account',
+          username: 'Username',
+          password: 'Password',
+          remember: 'If you forgot your password contact your local admin or email info@voicesofyouth.org',
+          btnLogin: 'Login',
+        },
+        register: {
+          title: 'I\'m new here',
+          subtitle: 'Create a new account',
+          name: 'Name',
+          username: 'Username',
+          email: 'E-mail',
+          password: 'Password',
+          confirmPassword: 'Confirm password',
+          terms: 'Terms and conditions',
+          btnRegister: 'Register',
+          termsBox: {
+            title: 'Terms and Conditions',
+            description: 'Terms...',
+            refuse: 'Refuse',
+            accept: 'Accept',
+          },
+        },
+      },
     },
   },
 };
