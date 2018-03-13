ac44fc6d89d15320890ea98a69a75571a462b409
Elton Pereira de Lima <eltonplima@gmail.com>
2018-03-13 06:38:20 -0300

    Update graphql to use relay.

--- voicesofyouth/project/schema.py
+++ voicesofyouth/project/schema.py
@@ -1,7 +1,9 @@
 import graphene
+from graphene import relay, ObjectType, AbstractType
 
 from django.contrib.gis.db.models.fields import PolygonField
 from graphene_django.types import DjangoObjectType
+from graphene_django.filter import DjangoFilterConnectionField
 from graphene_django.converter import convert_django_field
 from taggit.managers import TaggableManager
 
@@ -10,37 +12,20 @@ from voicesofyouth.project.models import Project
 
 @convert_django_field.register(PolygonField)
 def polygon_to_graphene(field, registry=None):
-    # print(field)
     return graphene.JSONString()
 
 @convert_django_field.register(TaggableManager)
 def tags_to_graphene(field, registry=None):
-    # print(dir(field))
     return graphene.String()
 
 
-class ProjectType(DjangoObjectType):
+class ProjectNode(DjangoObjectType):
     class Meta:
         model = Project
+        filter_fields = ['name', 'themes']
+        interfaces = (relay.Node, )
 
 
-class Query():
-    all_projects = graphene.List(ProjectType)
-    project = graphene.Field(ProjectType,
-                             id=graphene.Int(),
-                             name=graphene.String())
-
-    def resolve_all_projects(self, info, **kwargs):
-        return Project.objects.all()
-
-    def resolve_project(self, info, **kwargs):
-        id = kwargs.get('id')
-        name = kwargs.get('name')
-
-        if id is not None:
-            return Project.objects.get(pk=id)
-
-        if name is not None:
-            return Project.objects.get(name=name)
-        
-        return None
+class Query(AbstractType):
+    all_projects = DjangoFilterConnectionField(ProjectNode)
+    project = relay.Node.Field(ProjectNode)
--- voicesofyouth/theme/schema.py
+++ voicesofyouth/theme/schema.py
@@ -1,8 +1,11 @@
 import graphene
+from graphene import relay, ObjectType, AbstractType
 
 from django.contrib.gis.db.models.fields import PolygonField
 from graphene_django.types import DjangoObjectType
 from graphene_django.converter import convert_django_field
+from graphene_django.filter import DjangoFilterConnectionField
+
 from taggit.managers import TaggableManager
 
 from voicesofyouth.theme.models import Theme
@@ -10,37 +13,20 @@ from voicesofyouth.theme.models import Theme
 
 @convert_django_field.register(PolygonField)
 def polygon_to_graphene(field, registry=None):
-    # print(field)
     return graphene.JSONString()
 
 @convert_django_field.register(TaggableManager)
 def tags_to_graphene(field, registry=None):
-    # print(dir(field))
     return graphene.String()
 
 
-class ThemeType(DjangoObjectType):
+class ThemeNode(DjangoObjectType):
     class Meta:
         model = Theme
+        filter_fields = ['name', 'project']
+        interfaces = (relay.Node, )
 
 
-class Query():
-    all_themes = graphene.List(ThemeType)
-    theme = graphene.Field(ThemeType,
-                           id=graphene.Int(),
-                           name=graphene.String())
-
-    def resolve_all_themes(self, info, **kwargs):
-        return Theme.objects.all()
-
-    def resolve_theme(self, info, **kwargs):
-        id = kwargs.get('id')
-        name = kwargs.get('name')
-
-        if id is not None:
-            return Theme.objects.get(pk=id)
-
-        if name is not None:
-            return Theme.objects.get(name=name)
-        
-        return None
+class Query(AbstractType):
+    all_themes = DjangoFilterConnectionField(ThemeNode)
+    theme = relay.Node.Field(ThemeNode)
