53b262a441a546d7b7a486b48f06d3f935dc90f5
Eduardo Douglas <edudouglas@gmail.com>
2018-03-27 14:24:22 -0300

    Fix admin notifications

--- voicesofyouth/core/context_processors.py
+++ voicesofyouth/core/context_processors.py
@@ -3,8 +3,9 @@ from voicesofyouth.report.models import ReportNotification
 
 
 def notifications(request):
-    if 'admin' in request.path:
+    if 'admin' in request.path and hasattr(request.user, 'projects'):
         return {'notifications': ReportNotification.objects
                 .filter(status__in=[NOTIFICATION_STATUS_PENDING, NOTIFICATION_STATUS_REVALUTION])
-                .filter(read=False).order_by('-modified_on').distinct()}
+                .filter(read=False)
+                .filter(report__theme__project__in=request.user.projects).order_by('-modified_on').distinct()}
     return {}
