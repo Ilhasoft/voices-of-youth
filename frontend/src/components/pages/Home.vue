cf4212f9e2938be26b706ceef0cc9383e6121737
Eduardo Douglas <edudouglas@gmail.com>
2018-05-02 17:47:34 -0300

    Implementing new home

--- frontend/src/components/pages/Home.vue
+++ frontend/src/components/pages/Home.vue
@@ -6,7 +6,13 @@
       </div>
 
       <div class="slide">
-        <img src="~@/assets/img/slide-home.png" alt="">
+        <carousel :per-page="1" paginationColor="#009ee3" :mouse-drag="true">
+          <slide><img src="~@/assets/img/slide-home.png" alt=""></slide>
+          <slide><img src="~@/assets/img/slide-home.png" alt=""></slide>
+          <slide><img src="~@/assets/img/slide-home.png" alt=""></slide>
+          <slide><img src="~@/assets/img/slide-home.png" alt=""></slide>
+        </carousel>
+        <!-- <img src="~@/assets/img/slide-home.png" alt=""> -->
       </div>
     </div>
 
@@ -92,6 +98,94 @@
           <h1>Explore where young<br/>people are mapping</h1>
         </div>
       </div>
+
+      <div class="projects">
+        <div class="columns is-marginless" v-for="item in [0, 1]" :key="item">
+          <div class="column is-10 is-offset-1">
+            <div class="columns">
+              <div class="column" v-for="item in items" :key="item">
+                <div class="is-paddingless box">
+                  <img src="~@/assets/img/report-example-1.png" alt="">
+                  <div class="text">
+                    <h4>Mongólia</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="has-text-centered see-all">
+          <a href="#">See all</a>
+        </div>
+      </div>
+    <!-- // end container -->
+    </div>
+
+    <div class="columns is-marginless about-voy">
+      <div class="column is-offset-4">
+        <div class="text">
+          <h1>Voices of youth</h1>
+          <p>Voices of Youth offers inspiring, original insight and opinion from across the globe – from young people, for young people.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer">
+      <div class="columns is-marginless">
+        <div class="column is-4 is-offset-2">
+          <h4>Join us</h4>
+          <p>If you wish to become a mapper, fill out the form and we will get back to you</p>
+
+          <form action="" class="form">
+            <p>
+              <input type="text" name="" placeholder="Name" />
+            </p>
+            <p>
+              <input type="email" name="" placeholder="Email" />
+            </p>
+            <p>
+              <select name="" id="" class="select">
+                <option value="">What do you want?</option>
+                <option value="">I wanna be a mapper?</option>
+                <option value="">Questions or suggestions</option>
+              </select>
+            </p>
+            <p>
+              <select name="" id="" class="select">
+                <option value="">Choose a project?</option>
+              </select>
+            </p>
+            <p>
+              <textarea name="" id="" cols="30" rows="10"></textarea>
+            </p>
+            <div class="columns buttons">
+              <div class="column">
+                <div class="columns">
+                  <div class="column">
+                    <vue-recaptcha class="g-recaptcha" sitekey="6Lcp41YUAAAAAGNhDzBMW2ZzO33JJ6LdAY_aeE9g" />
+                  </div>
+                </div>
+                <div class="columns">
+                  <div class="column is-4 btn-submit">
+                    <button class="submit">Send</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div class="column is-offset-1">
+          <!-- <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fvoicesofyouth&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=true&show_facepile=true&appId" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe> -->
+        </div>
+      </div>
+
+      <div class="columns terms">
+        <div class="column">
+          <p class="has-text-centered">Privacy policy and terms of use</p>
+        </div>
+      </div>
     </div>
     
     
@@ -131,10 +225,14 @@
 
 <script>
 import { mapActions, mapGetters } from 'vuex';
+import VueRecaptcha from 'vue-recaptcha';
+import { Carousel, Slide } from 'vue-carousel';
 import router from '@/router/';
 
 export default {
   name: 'Home',
+
+  components: { VueRecaptcha, Carousel, Slide },
 
   data() {
     return {
@@ -175,7 +273,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import url('https://fonts.googleapis.com/css?family=Roboto');
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,500');
 
 .header {
   width: 100%;
@@ -202,14 +300,11 @@ export default {
 
 .slide {
   text-align: center;
-
-  img {
-    height: 426px !important;
-  }  
 }
 
 .container {
-  padding: 30px 100px 0 100px;
+  // padding: 30px 100px 0 100px;
+  font-weight: 400;
 
   @media (max-width: 990px) {
     padding: 30px 0px 0px 0px;
@@ -330,102 +425,158 @@ export default {
     color: #000000;
     text-align: left;
   }
+
+  .projects {
+    .box {
+      border: 1px solid #c3c3c3;
+      border-radius: 8px;
+
+      .text {
+        font-family: 'Roboto';
+        color: #000000;
+        text-align: left;
+        padding: 18px;
+
+        h4 {
+          font-size: 18px;
+          font-weight: 500;
+        }
+
+        p {
+          margin-top: 36px;
+          font-size: 14px;
+        }
+      }
+
+      img {
+        border-top-left-radius: 8px;
+        border-top-right-radius: 8px;
+        width: 100%;
+      }
+    }
+
+    .see-all {
+      margin-top: 31px;
+      margin-bottom: 40px;
+
+      a {
+        font-size: 18px;
+        color: #4a90e2;
+      }
+    }
+  }
 }
 
-  // .m-auto {
-  //   margin: auto;
-  // }
+.about-voy {
+  color: #ffffff;
+  text-align: left;
+  background-image: url('~@/assets/img/home-bg-footer.png');
+  background-size: cover;
+  background-position: bottom;
+  width: 100%;
+  height: 305px;
 
-  // .header-info {
-  //   margin: auto;
-  //   background-color: #00cbff;
+  .text {
+    margin-left: 50px;
+    margin-top: 57px;
+    width: 26em;
+    word-wrap: break-word;
 
-  //   h1 {
-  //     line-height: normal;
-  //     font-size: 50px;
-  //     font-weight: bold;
-  //     letter-spacing: -1.3px;
-  //     text-align: left;
-  //     color: #fff;
-  //   }
+    h1 {
+      font-size: 55px;
+      font-weight: bold;
+    }
 
-  //   small {
-  //     font-size: 25px;
-  //     letter-spacing: -0.6px;
-  //     text-align: left;
-  //     color: #fff;
-  //   }
-
-  //   p {
-  //     margin-top: 25px;
-  //     font-size: 15px;
-  //     line-height: 1.67;
-  //     letter-spacing: -0.4px;
-  //     text-align: left;
-  //     color: #fff;
-  //   }
-
-  //   .img-home {
-  //     width: 417px;
-  //     height: 400px;
-  //     object-fit: contain;
-  //   }
-  // }
-
-.body {
-  margin: auto;
-  background-color: #fff;
-  padding-top: 30px;
-  padding-left: 4.2em;
-
-  .m-bottom {
-    padding-bottom: 40px;
+    p {
+      font-size: 16px;
+    }
   }
 
-  .image {
-    width: 139px;
+  @media (max-width: 990px) {
+    .text {
+      width: 19rem;
+      margin-top: 0px;
+      margin-top: 0px;
+    }
+  }
+}
+
+.footer {
+  background-color: #009ee3;
+  color: #ffffff;
+  text-align: left;
+  padding-bottom: 0px;
+
+  h4 {
+    font-size: 40px;
+    font-weight: bold;  
   }
 
-  .p-top {
-    padding-top: 0px;
+  p {
+    font-size: 16px;
   }
 
-  h1 {
-    font-size: 35px;
-    font-weight: bold;
-    letter-spacing: -0.9px;
-    text-align: left;
-    color: #6c17b5;
-    margin-bottom: 15px;
+  .form {
+    margin-top: 40px;
+
+    input, select, textarea {
+      margin: 10px 0px 10px 0px;
+      background-color: #ffffff;
+      outline: none;
+      border: none;
+    }
+
+    input[type='text'], input[type='email'] {
+      border-radius: 4px;
+      height: 37px;
+      width: 100%;
+      padding-left: 5px;
+      font-size: 18px;
+      color: #7b7b7b;
+    }
+
+    .select {
+      height: 37px;
+      width: 100%;
+      
+      border-radius: 8px;
+      font-size: 18px;
+      color: #7b7b7b;
+    }
+
+    textarea {
+      height: 83px;
+      width: 100%;
+      border-radius: 4px;
+      padding: 5px;
+      resize: none;
+      font-size: 18px;
+      color: #7b7b7b;
+    }
+
+    .buttons {
+      margin-top: 40px;
+
+      .btn-submit {
+        text-align: right;
+
+        button {
+          height: 50px;
+          width: 100%;
+          background-color: #0379ac;
+          border-radius: 8px;
+          border: none;
+          font-family: 'Roboto';
+          font-size: 16px;
+          font-weight: bold;
+          color: #ffffff;
+        }
+      }
+    }
   }
 
-  img {
-    width: 139px;
-    height: 139px;
-  }
-
-  h2 {
-    font-size: 24px;
-    font-weight: 500;
-    letter-spacing: -0.6px;
-    text-align: left;
-    color: #000000;
-  }
-
-  small {
-    font-size: 15px;
-    letter-spacing: -0.4px;
-    text-align: left;
-    color: #263238;
-  }
-
-  .button {
-    margin-top: 10px;
-    display: block;
-    width: 115px;
-    height: 41px;
-    border-radius: 100px;
-    border: solid 2px #00cbff;
+  .terms {
+    margin-top: 145px;
   }
 }
 </style>
