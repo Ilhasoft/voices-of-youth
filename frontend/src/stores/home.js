7b2c53bc7b932fcbc76d78a4ab810f9ad010937d
Eduardo Douglas <edudouglas@gmail.com>
2018-05-07 16:48:24 -0300

    Removed comments

--- frontend/src/stores/home.js
+++ frontend/src/stores/home.js
@@ -1,43 +1,6 @@
 import axios from 'axios';
 
 export default {
-//   state: {
-//     all: [],
-//     current: {},
-//     language: '',
-//     disclaimer: true,
-//   },
-
-//   getters: {
-//     getAllProjects: state => state.all,
-//     getCurrentProject: state => state.current,
-//     getDisclaimerProject: state => state.disclaimer,
-//     getProjectLanguages: state => state.current.languages,
-//     getCurrentLanguage: state => state.language,
-//   },
-
-  /* eslint-disable no-param-reassign */
-//   mutations: {
-//     [TYPES.SET_PROJECTS_LIST](state, obj) {
-//       state.all = obj;
-//     },
-
-//     [TYPES.SET_CURRENT_PROJECT](state, obj) {
-//       if (obj) {
-//         state.current = obj;
-//         document.title = `Voices of Youth - ${obj.name}`;
-//       }
-//     },
-
-//     [TYPES.SET_DISCLAIMER_PROJECT](state, obj) {
-//       state.disclaimer = obj;
-//     },
-
-//     [TYPES.SET_CURRENT_LANGUAGE](state, obj) {
-//       state.language = obj;
-//     },
-//   },
-
   actions: {
     getHomeSlide: async () => {
       const data = await axios.get('/api/home-slide');
