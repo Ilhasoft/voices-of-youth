4b095a29f5f0826b979d9578742268f5bae7a8fa
Eduardo Douglas <edudouglas@gmail.com>
2018-06-05 16:36:12 -0300

    Removed basic authentication

--- voicesofyouth/settings.py
+++ voicesofyouth/settings.py
@@ -203,9 +203,7 @@ STATICFILES_DIRS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
