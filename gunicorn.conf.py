9547d352b7e5a79ec3683ed13e4fa12a7fb2d9ec
eltonplima <eltonplima@gmail.com>
2018-06-12 17:10:08 -0300

    Added gunicorn configuration file.

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+import os
+import multiprocessing
+
+current_path = os.path.dirname(os.path.abspath(__file__))
+bind = '0.0.0.0:8000'
+workers = multiprocessing.cpu_count() * 2
+name = 'rapidpro'
+env = 'DJANGO_SETTINGS_MODULE=voicesofyouth.settings'
+proc_name = 'voy'
+default_proc_name = proc_name
+chdir = current_path
+loglevel = 'info'
+timeout = 120
